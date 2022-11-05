ff6f669831b3315754069490fbbc45f44034a655
TanJadhav <87604486+TanJadhav@users.noreply.github.com>
2022-11-05 14:15:07 +0530

    code changes

---
+++ src/main/java/com/execute/OnlineCompilerApplication.java
@@ -0,0 +1,13 @@
+package com.execute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class executeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(executeApplication.class, args);
+	}
+
+}
